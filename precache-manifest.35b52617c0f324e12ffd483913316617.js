8f180e997dd92414468dd8ed52b08ffa563cb467
Jacek Dynski <jacek.dynski@gmail.com>
2020-01-23 22:54:05 +0100

    Updates

--- precache-manifest.35b52617c0f324e12ffd483913316617.js
+++ precache-manifest.35b52617c0f324e12ffd483913316617.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9de30be08ef5e702ef3594f135a2d4ec",
+    "revision": "0c3caf32a39ebfad0b4e5f1a27e3cbb8",
     "url": "/resume/index.html"
   },
   {
-    "revision": "4d0e16f83957c2db6822",
+    "revision": "725d7b1925607fe472e6",
     "url": "/resume/static/css/main.8ec0ca04.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/resume/static/js/2.47b0cd07.chunk.js"
   },
   {
-    "revision": "4d0e16f83957c2db6822",
-    "url": "/resume/static/js/main.47751ac1.chunk.js"
+    "revision": "725d7b1925607fe472e6",
+    "url": "/resume/static/js/main.9ada8d20.chunk.js"
   },
   {
     "revision": "5abcddc2bc339de040f9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.1bd63efbb2cbdfd18e50794956a919f2.js"
+  "/resume/precache-manifest.35b52617c0f324e12ffd483913316617.js"
 );
 
 self.addEventListener('message', (event) => {
