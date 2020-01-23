6fb35a856b757c51b0cefe5aa46a8fadafb722e9
Jacek Dynski <jacek.dynski@gmail.com>
2020-01-23 22:31:52 +0100

    Updates

---
+++ precache-manifest.1bd63efbb2cbdfd18e50794956a919f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de30be08ef5e702ef3594f135a2d4ec",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "4d0e16f83957c2db6822",
+    "url": "/resume/static/css/main.8ec0ca04.chunk.css"
+  },
+  {
+    "revision": "e6efa846f3d8bc3bc047",
+    "url": "/resume/static/js/2.47b0cd07.chunk.js"
+  },
+  {
+    "revision": "4d0e16f83957c2db6822",
+    "url": "/resume/static/js/main.47751ac1.chunk.js"
+  },
+  {
+    "revision": "5abcddc2bc339de040f9",
+    "url": "/resume/static/js/runtime-main.fa398bf3.js"
+  }
+]);
