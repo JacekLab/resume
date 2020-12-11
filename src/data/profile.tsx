e9e144caea07588992da83875d278783f6c75ebe
JacekLab <jacek.dynski@gmail.com>
2020-12-11 22:19:37 +0100

    move application data from html to const; create interfaces

--- src/data/profile.tsx
+++ src/data/profile.tsx
@@ -1,22 +1,22 @@
-import React, { ReactElement } from "react";
+import React from "react";
 
-interface About {
+export interface AboutInterface {
   name: string,
-  description: ReactElement
+  description: JSX.Element
 };
-export const ABOUT: About = {
+export const ABOUT: AboutInterface = {
   name: "Jacek Dyński",
   description: <p>My main area of expertize is web development and design. I have experience in building business single page web applications in React.js. My core focus is modern JavaScript, React exosystem, and PWA with Gatsby. <br /> I am also interested in usability, accessibility and design systems. I like to experiment with SVG, canvas and animations.</p>,
 };
 
-interface MainInfo {
+export interface MainInfoInterface {
   email: string,
   github: string,
   jobTitle: string,
   languages: Array<string>,
   location: string
 };
-export const MAIN_INFO: MainInfo = {
+export const MAIN_INFO: MainInfoInterface = {
   email: "jacek.dynski@gmail.com",
   github: "https://github.com/JacekLab",
   jobTitle: "web developer, designer",
@@ -24,12 +24,12 @@ export const MAIN_INFO: MainInfo = {
   location: "PL, Opole"
 }
 
-interface Technologies {
-  know: Array<string>,
+export interface TechnologiesInterface {
+  know: string[],
   learning: Array<string>,
   familiar: Array<string>,
 };
-export const TECHNOLOGIES: Technologies = {
+export const TECHNOLOGIES: TechnologiesInterface = {
   know: [
     "React",
     "Webpack",
@@ -51,3 +51,65 @@ export const TECHNOLOGIES: Technologies = {
     "SharePoint 2016",
   ]
 };
+
+export interface WorkExperience {
+  dates: string,
+  role: string,
+  company: string,
+  location: string,
+}
+export const EXPERIENCE: Array<WorkExperience> = [
+  {
+    dates: '09.2012-06.2013',
+    role: 'EVS Volunteer',
+    company: 'Association ALEM',
+    location: 'Portugal, Lisbon'
+  },
+  {
+    dates: '12.2013-12.2016',
+    role: 'Editor in Design Services',
+    company: 'Adecco Consulting',
+    location: 'Poland, Wroclaw'
+  },
+  {
+    dates: '01.2017-12.2018',
+    role: 'Graphic designer',
+    company: 'Credit Suisse',
+    location: 'Poland, Wroclaw'
+  },
+  {
+    dates: '01.2019-12.2020',
+    role: 'Web developer',
+    company: 'Credit Suisse',
+    location: 'Poland, Wroclaw'
+  },
+];
+
+export interface EducationItem {
+  dates: string,
+  school: string,
+  faculty: string,
+}
+export const EDUCATION: Array<EducationItem> = [
+  {
+    dates: '2009-2011',
+    school: 'Jagiellonian University',
+    faculty: 'Master of Cultural Studies',
+  },
+  {
+    dates: '2007-2009',
+    school: 'University of Opole',
+    faculty: 'Bachelor of Cultural Studies',
+  },
+]
+
+export const COURSES: Array<string> = [
+  'Professional Scrum Master I, Altkom Akademia',
+  'Reactive Programming with RxJS, Altkom Akademia',
+  'Advanced JavaScript aspects, Altkom Akademia',
+  'UX/UI, Akademia UX/UI',
+  'SPA with React.js and Redux, Altkom Akademia',
+  'Animations with Anime.js and CSS 3, Altkom Akademia',
+  'Project Management Essentials, Energy Group',
+  'Project Risk Management, Energy Group',
+]
