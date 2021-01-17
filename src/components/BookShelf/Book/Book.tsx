40dd3a1378c53e3862dca6c98ca9e9b011c4792b
JacekLab <jacek.dynski@gmail.com>
2021-01-17 23:09:42 +0100

    simplified adjust long book title

--- src/components/BookShelf/Book/Book.tsx
+++ src/components/BookShelf/Book/Book.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from "react";
-import { BookInterface, BookSize } from "../../../types/index";
+import { BookInterface, HandleBookFunc } from "../../../types/index";
 import "./Book.css";
 
 interface BookProps {
@@ -9,23 +9,11 @@ interface BookProps {
   bookWidth: string;
   coverStyles: React.CSSProperties;
   bookHeight: any;
-  handleBook: any;
+  handleBook: HandleBookFunc;
 }
 
 const Book: React.FC<BookProps> = (props: BookProps) => {
   const { book, bookWidth, coverStyles, bookHeight, handleBook } = props;
-
-  const getAdjustTitle = (bookSize: BookSize): string => {
-    switch (bookSize) {
-      case "S":
-      default:
-        return book.title.slice(0, 17);
-      case "M":
-        return book.title.slice(0, 22);
-      case "L":
-        return book.title.slice(0, 24);
-    }
-  };
 
   return (
     <div className="BookWrapper" style={{ width: bookWidth, height: bookHeight }}>
@@ -37,8 +25,7 @@ const Book: React.FC<BookProps> = (props: BookProps) => {
         onKeyDown={() => handleBook(book, "click")}
         className="BookTitle"
       >
-        {getAdjustTitle(bookHeight)}
-        {getAdjustTitle(bookHeight).length < book.title.length ? " ..." : null}
+        {book.title.length > 24 ? `${book.title.slice(0, 22)} . . .` : book.title}
       </div>
     </div>
   );
